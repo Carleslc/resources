f0cec856bcfb1e49ef81efb5716bde16bc957b02
Carleslc <lazaro.costa.carles@gmail.com>
2019-05-09 22:31:58 +0200

    Display image and open add form url from script

--- get_info.py
+++ get_info.py
@@ -3,15 +3,22 @@
 
 import json
 import argparse
+import pyperclip
+import webbrowser
 from requests import get
 from bs4 import BeautifulSoup
+from urllib.parse import urlencode
 from colorama import Fore, Style, init
 from PIL import Image
+
+RESOURCES_URL = "https://airtable.com/shrnzLIolsKJMD9Ql"
 
 def set_args():
     global args
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="website URL")
+    parser.add_argument("--display", action='store_true', help="set if you want to show website og:image")
+    parser.add_argument("--add", action='store_true', help="open the resource list form to add this website")
     args = parser.parse_args()
 
 def print_colored(message, *colors):
@@ -40,6 +47,24 @@ def get_source(url):
         exit(1)
     return BeautifulSoup(source.content, 'html.parser')
 
+def get_url(url, params):
+    return url + '?' + urlencode(params)
+
+def copy_display(image_url):
+    pyperclip.copy(image_url)
+    print_colored('Image URL copied to clipboard!', Style.DIM)
+    if args.display:
+        Image.open(get(image_url, stream=True).raw).show()
+
+def add_resource(title, link, description):
+    params = {
+        'prefill_Title': title,
+        'prefill_Link': link,
+        'prefill_Description': description
+    }
+    url = get_url(RESOURCES_URL, params)
+    webbrowser.open(url)
+
 if __name__ == "__main__":
     set_args()
 
@@ -47,10 +72,14 @@ if __name__ == "__main__":
     html = get_source(args.url)
     #print_colored(html.prettify(), Style.DIM)
     
-    print_meta("url", Style.DIM)
-    print(html.find("title").string)
+    url = print_meta("url", Style.DIM)
+    site_name = print_meta("site_name", Style.BRIGHT)
+    title = print(html.find("title").string)
     print_meta("title", Fore.GREEN)
-    print_meta("description", Fore.CYAN)
+    description = print_meta("description", Fore.CYAN)
     image = print_meta("image", Fore.YELLOW)
     if image:
-        Image.open(get(image, stream=True).raw).show()
+        copy_display(image)
+
+    if args.add:
+        add_resource(site_name or title, url or args.url, description)
